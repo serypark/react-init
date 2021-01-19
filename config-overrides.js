90c7e9eb84a2635379a6c2f0abf196f91f989461
Jeongseok0914 <jeongseok0914@gmail.com>
2021-01-19 14:20:17 +0900

    Update : SVG 로더 변경
    변경

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,6 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable no-param-reassign */
-/* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path');
-const rewireSvgReactLoader = require('react-app-rewire-svg-react-loader');
+const { override } = require('customize-cra');
 
-module.exports = function override(config, env) {
-  config = rewireSvgReactLoader(config, env);
-  return config;
-};
+
+module.exports = override(
+  
+);
